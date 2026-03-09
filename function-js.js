9e8116c68ec0c0413ec05e7c017eb3989f278450
Blackjack234 <web.paulanirban@gmail.com>
2026-03-09 20:17:58 +0530

    more function

--- function-js.js
+++ function-js.js
@@ -83,3 +83,78 @@ const fun = function (){
 fun() //Output: undefined
 
 //Explanation: In the function fun, the variable value is declared with var, which is hoisted to the top of the function scope. However, the assignment of value to 20 happens after the console.log statement. Therefore, when console.log(value) is executed, it accesses the hoisted variable value, which is undefined at that point.
+
+
+// param vs arguments   
+
+function sum2(a, b) { // a and b are parameters
+    return a + b;
+}
+
+console.log(sum2(5, 3),"sum2"); // 5 and 3 are arguments
+
+
+//spread vs rest opatrator
+
+//Spread operator
+const arr1 = [1, 2, 3];
+const arr2 = [...arr1, 4, 5]; // Using spread operator to create a new array
+console.log(arr2); // Output: [1, 2, 3, 4, 5]
+
+
+function numbers(a,x,y,...number){ //rest operator
+console.log(x,y,number);
+
+}
+
+
+numbers(2,3,4,5,6,7,8,9)
+
+//What is callback function
+//A callback function is a function that is passed as an argument to another function and is executed after some operation has been completed. It allows you to handle asynchronous operations or customize the behavior of a function.
+
+//Example of a callback function
+function fetchData(callback) {
+    setTimeout(function() {
+        const data = "Data fetched from server";
+        callback(data); // Calling the callback function with the fetched data
+    }, 2000); // Simulating an asynchronous operation with a delay of 2 seconds
+}
+
+function displayData(data) {
+    console.log(data); // Output: Data fetched from server
+}
+
+fetchData(displayData); // Passing the displayData function as a callback to fetchData
+
+
+// Arrow function
+const add3 = (a, b) => a + b;
+
+console.log(add3(5, 3)); // Output: 8
+
+//Arrow function with multiple statements
+const multiply = (a, b) => {
+    const result = a * b;
+    return result;
+};
+
+console.log(multiply(5, 3)); // Output: 15
+
+// Arrow function vs normal function
+
+//Normal function
+function greet3(name) {
+    console.log("Hello, " + name + "!");
+}
+
+greet3("Alice"); // Output: Hello, Alice!
+
+//Arrow function
+const greet4 = (name) => {
+    console.log("Hi, " + name + "!");
+};
+
+greet4("Bob"); // Output: Hi, Bob!
+
+//In the normal function greet3, we use the function keyword to define the function. In the arrow function greet4, we use the arrow syntax (=>) to define the function. Arrow functions are more concise and do not have their own this context, which can be beneficial in certain situations, such as when working with callbacks or methods in objects.  
